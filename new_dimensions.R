3e7f8ae4025f0d353f695f373ec6eb1eb3e88604
Lewis Sternberg <lewiss@devinit.org>
2020-05-19 15:24:50 +0100

    merge update
    still to limit countries used for ranks

--- new_dimensions.R
+++ new_dimensions.R
@@ -6,6 +6,7 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only=T)
 setwd("C:/git/CBLB_COVID/")
+lookup <- fread("data/lookup.csv")
 
 ####INFORM####
 #source: https://drmkc.jrc.ec.europa.eu/inform-index/INFORM-Epidemic
@@ -14,6 +15,7 @@ type <- ifelse(nms=="EPIDEMIC INFORM RISK","numeric","guess")
 INFORM <- setDT(read_xlsx("data/INFORM.xlsx",sheet="INFORM Epi 2020 (a-z)",col_types=type,))
 INFORM <- INFORM[-1,c(1,2,44)]
 INFORM$rank <- rank(-INFORM[,3],ties.method = "min")
+names(INFORM)[2] <-"iso3c"
 
 ####WB####
 #population aged 65 and above - SP.POP.65UP.TO.ZS (less is better)
@@ -43,6 +45,9 @@ sleeping <- data.table(sleeping$Data)
 sleeping <- sleeping[sleeping[,.I[SurveyYear==max(SurveyYear)],by=CountryName]$V1]
 sleeping$rank <- rank(-sleeping$Value,ties.method="min")
 sleeping <- select(sleeping, CountryName,SurveyYear, Value, rank)
+sleeping$CountryName[which(sleeping$CountryName=="Congo Democratic Republic")]="Congo, Democratic Republic of"
+sleeping <- merge(sleeping, lookup[,c(1,3)],by.x="CountryName",by.y="Country Name",all.x=T)
+names(sleeping)[which(names(sleeping)=="ISO 3")] <- "iso3c"
 
 ####universal health coverage index####
 UHC <- setDT(read.csv("http://apps.who.int/gho/athena/api/GHO/UHC_INDEX_REPORTED/?format=csv"))
@@ -54,6 +59,10 @@ UHC$rank <- rank(UHC$value,ties.method="min")
 ####Global Food Security Index####
 GFSI <- fread("data/GFSI.csv")
 GFSI$rank <- rank(GFSI$Score,ties.method = "min")
+GFSI$Country[which(GFSI$Country=="Congo (Dem. Rep.)")]="Congo, Democratic Republic of"
+GFSI$Country[which(GFSI$Country=="Cote d’Ivoire")]="Cote d'Ivoire"
+GFSI <- merge(GFSI, lookup[,c(1,3)],by.x="Country",by.y="Country Name",all.x=T)
+names(GFSI)[which(names(GFSI)=="ISO 3")] <- "iso3c"
 
 ####Prevaslence of raised blood pressure####
 hypertension <- setDT(read.csv("http://apps.who.int/gho/athena/api/GHO/BP_03/?format=csv"))
@@ -75,7 +84,8 @@ for(i in 1:length(urls)){
     html_table()
   dat <- dat[[1]]
   country <- html_nodes(html,xpath='//*[@id="idf-print-area"]/div[1]/div[2]/h1') %>%
-   html_text()
+   html_text()%>%
+    trimws("both")
   if(dat[4,3]=="-"){ #using 2010 data when 2019 data is missing
     dataList[[dataIndex]] <- as.data.table(cbind(country,as.numeric(dat[4,2])))
   }else{dataList[[dataIndex]] <- as.data.table(cbind(country,as.numeric(dat[4,3])))
@@ -87,18 +97,51 @@ diabetes <- rbindlist(dataList)
 names(diabetes)[2] <- "prevalence"
 diabetes$prevalence <- as.numeric(diabetes$prevalence)
 diabetes$rank <- rank(-diabetes$prevalence, ties.method = "min")
+diabetes[country=="Macau"]$country<-"China, Macao SAR"
+diabetes[country=="US Virgin Islands"]$country<-"Virgin Islands, US"
+diabetes <- merge(diabetes, lookup[,c(1,3)],by.x="country",by.y="Country Name",all.x=T)
+names(diabetes)[which(names(diabetes)=="ISO 3")] <- "iso3c"
 
 ####IHME####
 IHME <- fread("data/IHME.csv")
+IHME[location=="Virgin Islands, U.S."]$location<-"Virgin Islands, US"
+IHME[location=="Taiwan (Province of China)"]$location<-"Taiwan Province of China"
+IHME <- merge(IHME, lookup[,c(1,3)],by.x="location",by.y="Country Name",all.x=T)
+names(IHME)[which(names(IHME)=="ISO 3")] <- "iso3c"
 #Chronic respiratory diseases
-respiratory <- IHME[IHME[cause=="Chronic respiratory diseases"&metric=="Percent",.I[year==max(year)],by=location]$V1][,c(2,7,8)]
+respiratory <- IHME[IHME[cause=="Chronic respiratory diseases"&metric=="Percent",.I[year==max(year)],by=location]$V1][,c(2,7,8,11)]
 respiratory$rank <- rank(-respiratory$val,ties.method="min")
 #Cardiovascular diseases
-cardio <- IHME[IHME[cause=="Cardiovascular diseases"&metric=="Percent",.I[year==max(year)],by=location]$V1][,c(2,7,8)]
+cardio <- IHME[IHME[cause=="Cardiovascular diseases"&metric=="Percent",.I[year==max(year)],by=location]$V1][,c(2,7,8,11)]
 cardio$rank <- rank(-cardio$val,ties.method="min")
 
 ####WFP####
 #Source: https://dataviz.vam.wfp.org/global-coverage-proteus-food-security-index-oct-2019
 WFP <- fread("data/proteus_2017.csv")
+WFP[Country=="Lao Pdr"]$Country <- "Lao"
+WFP[Country=="Gambia, The"]$Country <- "The Gambia"
+WFP[Country=="Macao Sar, China"]$Country <- "China, Macao SAR"
+WFP[Country=="Hong Kong Sar, China"]$Country <- "Hong Kong"
+WFP[Country=="Venezuela, Rb"]$Country <- "Venezuela"
+WFP[Country=="Macedonia, Fyr"]$Country <- "Macedonia"
+WFP[Country=="Bahamas, The"]$Country <- "The Bahamas"
+WFP[Country=="St. Vincent and The Grenadines"]$Country <- "St. Vincent and the Grenadines"
+WFP <- merge(WFP, lookup[,c(1,3)],by.x="Country",by.y="Country Name",all.x=T)
+names(WFP)[which(names(WFP)=="ISO 3")] <- "iso3c"
 WFP$rank <- rank(-WFP$`Proteus index`, ties.method="min")
-WFP <- WFP[,c(1,6,8)]
+WFP <- WFP[,c(1,6,8,9)]
+
+####merge####
+#only keeps rank and iso3c
+dats <- c("WFP","cardio","diabetes","hypertension","GFSI","UHC","sleeping",WB_indicators,"INFORM")
+for(dat in dats){
+  temp <- get(dat)
+  temp <- select(temp,iso3c,rank)
+  names(temp)[which(names(temp)=="rank")] <- paste0(dat,"_rank")
+  assign(dat,temp)
+}
+indicators_ranks <- lookup[!duplicated(lookup$`ISO 3`)][,c(1,3)]
+names(indicators_ranks)[which(names(indicators_ranks)=="ISO 3")] <- "iso3c"
+indicators_ranks <- Reduce(function(x,y) merge(x,y, by="iso3c",all.x=T),
+                    list(indicators_ranks ,WFP,cardio,diabetes,hypertension,GFSI,UHC,sleeping,INFORM,SP.POP.65UP.TO.ZS,
+                         SP.URB.TOTL.IN.ZS, SH.STA.HYGN.ZS,SH.MED.BEDS.ZS, SH.MED.PHYS.ZS))
